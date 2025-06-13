95f75ce54360ed39f9cecf6cb67f98b70747bf93
tsabita <tsabitaamaliahs@gmail.com>
2025-06-13 20:45:40 +0700

    menambahkan role pada model

--- user-service/app/Models/User.php
+++ user-service/app/Models/User.php
@@ -35,6 +35,7 @@ class User extends Authenticatable implements JWTSubject
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
+            'role'  => $this->role,
         ];
     }
 }
